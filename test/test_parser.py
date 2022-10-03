81e977e0c057c518e7f2499f22d94e059c2f2511
Ryu juheon <saidbysolo@gmail.com>
2022-10-04 00:13:34 +0900

    test(parser): add parse test

--- test/test_parser.py
+++ test/test_parser.py
@@ -1,3 +1,4 @@
+from dataclasses import asdict
 from math import nan
 import parser
 
@@ -33,3 +34,143 @@ def test_is_alphabet():
     assert parser.is_alphabet("Z") is False
     assert parser.is_alphabet("0") is False
     assert parser.is_alphabet("-") is False
+
+
+def test_parse():
+    # Title with numeric episode number
+    assert asdict(parser.parse("Title - 1 (CH 1920x1080 x264 AAC)")) == {
+        "title": {
+            "original": "Title",
+            "seasonal": "Title",
+            "series": "",
+        },
+        "file": {
+            "name": "Title - 1 (CH 1920x1080 x264 AAC)",
+            "resolution": "1920x1080",
+            "codec": {
+                "video": "x264",
+                "audio": "AAC",
+            },
+        },
+        "provider": {
+            "channel": "CH",
+        },
+        "episodes": [1],
+        "seasons": [1],
+    }
+    # Title with numeric episode number and season number
+    assert asdict(parser.parse("Title 2 - 1 (CH 1920x1080 x264 AAC)")) == {
+        "title": {"original": "Title", "seasonal": "Title 2", "series": ""},
+        "file": {
+            "name": "Title 2 - 1 (CH 1920x1080 x264 AAC)",
+            "resolution": "1920x1080",
+            "codec": {"video": "x264", "audio": "AAC"},
+        },
+        "provider": {"channel": "CH"},
+        "episodes": [1],
+        "seasons": [2],
+    }
+    # Title with two digit numeric episode number
+    assert asdict(parser.parse("Title - 01 (CH 1920x1080 x264 AAC)")) == {
+        "title": {"original": "Title", "seasonal": "Title", "series": ""},
+        "file": {
+            "name": "Title - 01 (CH 1920x1080 x264 AAC)",
+            "resolution": "1920x1080",
+            "codec": {"video": "x264", "audio": "AAC"},
+        },
+        "provider": {"channel": "CH"},
+        "episodes": [1],
+        "seasons": [1],
+    }
+    # Title with float type episode number
+    assert asdict(parser.parse("Title - 01.2 (CH 1920x1080 x264 AAC)")) == {
+        "title": {"original": "Title", "seasonal": "Title", "series": ""},
+        "file": {
+            "name": "Title - 01.2 (CH 1920x1080 x264 AAC)",
+            "resolution": "1920x1080",
+            "codec": {"video": "x264", "audio": "AAC"},
+        },
+        "provider": {"channel": "CH"},
+        "episodes": [1.2],
+        "seasons": [1],
+    }
+    # Title with series name and episode number
+    assert asdict(parser.parse("Title - Subtitle - 01 (CH 1920x1080 x264 AAC)")) == {
+        "title": {"original": "Title", "seasonal": "Title", "series": "Subtitle"},
+        "file": {
+            "name": "Title - Subtitle - 01 (CH 1920x1080 x264 AAC)",
+            "resolution": "1920x1080",
+            "codec": {"video": "x264", "audio": "AAC"},
+        },
+        "provider": {"channel": "CH"},
+        "episodes": [1],
+        "seasons": [1],
+    }
+    # Title with ranged episode number
+    assert asdict(parser.parse("Title - SUB2-9 (CH 1920x1080 x264 AAC)")) == {
+        "title": {"original": "Title", "seasonal": "Title", "series": "SUB"},
+        "file": {
+            "name": "Title - SUB2-9 (CH 1920x1080 x264 AAC)",
+            "resolution": "1920x1080",
+            "codec": {"video": "x264", "audio": "AAC"},
+        },
+        "provider": {"channel": "CH"},
+        "episodes": [2, 3, 4, 5, 6, 7, 8, 9],
+        "seasons": [1],
+    }
+    # Title with provider name
+    assert asdict(
+        parser.parse("[PROVIDER] Title - SUB2-9 (CH 1920x1080 x264 AAC)")
+    ) == {
+        "title": {"original": "Title", "seasonal": "Title", "series": "SUB"},
+        "file": {
+            "name": "[PROVIDER] Title - SUB2-9 (CH 1920x1080 x264 AAC)",
+            "resolution": "1920x1080",
+            "codec": {"video": "x264", "audio": "AAC"},
+        },
+        "provider": {"channel": "CH"},
+        "episodes": [2, 3, 4, 5, 6, 7, 8, 9],
+        "seasons": [1],
+    }
+    # Title with literal season
+    assert asdict(
+        parser.parse("[PROVIDER] Title 2nd season - SUB2-9 (CH 1920x1080 x264 AAC)")
+    ) == {
+        "title": {"original": "Title", "seasonal": "Title 2nd season", "series": "SUB"},
+        "file": {
+            "name": "[PROVIDER] Title 2nd season - SUB2-9 (CH 1920x1080 x264 AAC)",
+            "resolution": "1920x1080",
+            "codec": {"video": "x264", "audio": "AAC"},
+        },
+        "provider": {"channel": "CH"},
+        "episodes": [2, 3, 4, 5, 6, 7, 8, 9],
+        "seasons": [2],
+    }
+    # Title with no episode numbers (condensed)
+    assert asdict(
+        parser.parse("[PROVIDER] Title 2nd season (CH 1920x1080 x264 AAC)")
+    ) == {
+        "title": {"original": "Title", "seasonal": "Title 2nd season", "series": ""},
+        "file": {
+            "name": "[PROVIDER] Title 2nd season (CH 1920x1080 x264 AAC)",
+            "resolution": "1920x1080",
+            "codec": {"video": "x264", "audio": "AAC"},
+        },
+        "provider": {"channel": "CH"},
+        "episodes": [],
+        "seasons": [2],
+    }
+    # Title with episode name including ending note
+    assert asdict(
+        parser.parse("[PROVIDER] Title 2nd season - 2 END (CH 1920x1080 x264 AAC)")
+    ) == {
+        "title": {"original": "Title", "seasonal": "Title 2nd season", "series": ""},
+        "file": {
+            "name": "[PROVIDER] Title 2nd season - 2 END (CH 1920x1080 x264 AAC)",
+            "resolution": "1920x1080",
+            "codec": {"video": "x264", "audio": "AAC"},
+        },
+        "provider": {"channel": "CH"},
+        "episodes": [2],
+        "seasons": [2],
+    }
